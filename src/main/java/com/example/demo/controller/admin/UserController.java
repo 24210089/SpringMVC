f80c99d65fadcad7944acddef0f31f13a7e6614b
tran3tin <tran3tin@gmail.com>
2025-07-07 21:50:36 +0700

    #95

--- src/main/java/com/example/demo/controller/admin/UserController.java
+++ src/main/java/com/example/demo/controller/admin/UserController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller.admin;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,9 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 
 import com.example.demo.domain.User;
-import com.example.demo.domain.Role;
-import com.example.demo.service.UploadService;
 import com.example.demo.service.UserService;
+
+import jakarta.servlet.ServletContext;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,20 +25,11 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class UserController {
     private final UserService userService;
-    private final UploadService uploadService;
-    private final PasswordEncoder passwordEncoder;
+    private final ServletContext servletContext;
 
-    public UserController(UserService userService, UploadService uploadService, PasswordEncoder passwordEncoder) {
+    public UserController(UserService userService, ServletContext servletContext) {
         this.userService = userService;
-        this.uploadService = uploadService;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @GetMapping("/")
-    public String getHomePage(Model model) {
-        String message = userService.handleHello();
-        model.addAttribute("message", message);
-        return "hello";
+        this.servletContext = servletContext;
     }
 
     @GetMapping("/admin/user")
@@ -90,10 +84,7 @@ public class UserController {
 
     @RequestMapping("/admin/user/create")
     public String getUserPage(Model model) {
-        User newUser = new User();
-        Role newRole = new Role();
-        newUser.setRole(newRole);
-        model.addAttribute("newUser", newUser);
+        model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
 
@@ -101,13 +92,27 @@ public class UserController {
     public String createUserPage(Model model,
             @ModelAttribute("newUser") User hoidanit,
             @RequestParam("hoidanitFile") MultipartFile file) {
-        String avatar = this.uploadService.handleSaveUploadFile(file, "avatar");
-        String hashPassword = this.passwordEncoder.encode(hoidanit.getPassword());
 
-        hoidanit.setAvatar(avatar);
-        hoidanit.setPassword(hashPassword);
-        hoidanit.setRole(this.userService.getRoleByName(hoidanit.getRole().getName()));
+        // private final ServletContext servletContext;
+        byte[] bytes;
+        try {
+            bytes = file.getBytes();
 
+            String rootPath = this.servletContext.getRealPath("/resources/images");
+            File dir = new File(rootPath + File.separator + "avatar");
+            if (!dir.exists())
+                dir.mkdirs();
+            // Create the file on server
+            File serverFile = new File(dir.getAbsolutePath() + File.separator +
+                    +System.currentTimeMillis() + "-" + file.getOriginalFilename());
+            BufferedOutputStream stream = new BufferedOutputStream(
+                    new FileOutputStream(serverFile));
+            stream.write(bytes);
+            stream.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         this.userService.handleSaveUser(hoidanit);
         return "redirect:/admin/user";
     }
