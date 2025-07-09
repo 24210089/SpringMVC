e90f4528ed9fc5537e41f71ad701ceece199fcd8
tran3tin <tran3tin@gmail.com>
2025-07-09 20:55:43 +0700

    #99

--- src/main/java/com/example/demo/LaptopshopApplication.java
+++ src/main/java/com/example/demo/LaptopshopApplication.java
@@ -4,13 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(exclude = {
-    org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-    org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+    org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
 })
 public class LaptopshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LaptopshopApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,33 +1,30 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.UserService;
 
 
-// @Controller
-// public class UserController {
-
-//     @GetMapping("/")   
-//     public String getHomePage(){
-//         return "Hello from controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
+
     private UserService userService;
-    
+
+
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
 
+
     @GetMapping("/")   
-    public String getHomePage(){
-        return this.userService.handleHello();
+    public String getHomePage(Model model){
+        String test = this.userService.handleHello();
+        model.addAttribute("Eric", test);
+        model.addAttribute("hoidanit", "toi yeu ban");
+        return "hello";
     }
 }
